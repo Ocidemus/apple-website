2aac306942697e60d9b948760cf2f02ecc66ccaa
Aryan <codeofscendence@gmail.com>
2024-07-22 19:16:58 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { appleImg } from '../utils'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={appleImg} alt="apple" width={14} height={18} />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
